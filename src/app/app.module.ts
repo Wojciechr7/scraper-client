7a6293e4fa1f98943f29bbc17533ef87aa34008a
hr5t5@windowslive.com <hr5t5@windowslive.com>
2018-10-07 12:06:08 +0200

    import app module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { LayoutModule } from '@angular/cdk/layout';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavigationComponent} from './components/navigation/navigation.component';
+import {LayoutModule} from '@angular/cdk/layout';
 import {
     MatToolbarModule,
     MatButtonModule,
@@ -22,10 +22,10 @@ import {
     MatCardModule,
     MatTooltipModule
 } from '@angular/material';
-import { ProductsTableComponent } from './components/products-table/products-table.component';
-import { CartComponent } from './components/cart/cart.component';
-import {LoadingDialog, HomeComponent } from './components/home/home.component';
-import { DeviceDetectorModule } from 'ngx-device-detector';
+import {ProductsTableComponent} from './components/products-table/products-table.component';
+import {CartComponent} from './components/cart/cart.component';
+import {LoadingDialog, HomeComponent} from './components/home/home.component';
+import {DeviceDetectorModule} from 'ngx-device-detector';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {DataDialog, TableSettingsComponent} from './components/table-settings/table-settings.component';
@@ -40,48 +40,49 @@ const appRoutes: Routes = [
 ];
 
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent,
-    ProductsTableComponent,
-    CartComponent,
-    HomeComponent,
-    TableSettingsComponent,
-      DataDialog,
-      LoadingDialog
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-      RouterModule.forRoot(
-          appRoutes,
-          {enableTracing: false}
-      ),
-      HttpClientModule,
-      DeviceDetectorModule.forRoot(),
-      MatCheckboxModule,
-      MatProgressSpinnerModule,
-      MatDialogModule,
-      MatFormFieldModule,
-      FormsModule,
-      ReactiveFormsModule,
-      MatInputModule,
-      MatCardModule,
-      MatTooltipModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        NavigationComponent,
+        ProductsTableComponent,
+        CartComponent,
+        HomeComponent,
+        TableSettingsComponent,
+        DataDialog,
+        LoadingDialog,
+        ProductsTableComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        RouterModule.forRoot(
+            appRoutes,
+            {enableTracing: false}
+        ),
+        HttpClientModule,
+        DeviceDetectorModule.forRoot(),
+        MatCheckboxModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatCardModule,
+        MatTooltipModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
     entryComponents: [DataDialog, LoadingDialog]
 })
-export class AppModule { }
+export class AppModule {
+}
